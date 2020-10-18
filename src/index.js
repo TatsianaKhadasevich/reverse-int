934d7f3c8b6dba47266818a8d63796345fc0a7da
TatsianaKhadasevich <tatsyanakhodasevich@gmail.com>
2020-10-18 19:44:48 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let num = Math.abs(n).toString().split('');
+  let newNum=[];
+  for (let i=num.length-1;i>=0 ;i--){
+    newNum.push(num[i])
+  }
+  return Number(newNum.join(''));
 }
